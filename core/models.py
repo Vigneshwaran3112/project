f7621c46fd88a81b334eb7b7fa4b92e30543e965
kabil <kabil@vishgyana.com>
2021-01-28 15:08:29 +0530

    .

--- core/models.py
+++ core/models.py
@@ -144,7 +144,7 @@ class UserAttendanceBreak(BaseModel):
     user = models.ForeignKey(BaseUser, on_delete=models.CASCADE, related_name='user_attendances_break')
     start = models.DateTimeField()
     stop = models.DateTimeField(null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateField(auto_now_add=True)
     time_spend = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     def save(self, *args, **kwargs):
