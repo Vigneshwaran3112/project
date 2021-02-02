4d21d9b884d2e03c94c2de68947272b45f2e6ea9
kabil <kabil@vishgyana.com>
2021-02-02 15:38:04 +0530

    ...

--- south_indian_coffee_pro/asgi.py
+++ south_indian_coffee_pro/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'south_indian_coffee_pro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'south_indian_coffee_pro.settings.development_settings')
 
 application = get_asgi_application()
