926cf2c2a576e580bd356e341e2666643ad1b102
kabil <kabil@vishgyana.com>
2021-03-11 15:52:12 +0530

    .

--- core/serializers.py
+++ core/serializers.py
@@ -1246,14 +1246,13 @@ class DailySheetInventoryListSerializer(serializers.Serializer):
         branch = self.context['branch']
         date = self.context['date']
 
-        # data = InventoryControl.objects.get(branch__pk=branch, status=True, product__classification__code=2, date__date=date)
-
         inventory_list, bills_list = [],[]
         inventory_data =  {
                     'operational_products': {'id':2, 'name':"operational_products", 'completed_status':InventoryControl.objects.filter(branch__pk=branch, date__date=date, product__classification__code=2).exists()},
                     'raw_products':{'id':3, 'name':"raw_products", 'completed_status':InventoryControl.objects.filter(branch__pk=branch, date__date=date, product__classification__code=3).exists()},
-                    'food_wastage':{'id':4, 'name':"food_wastage", 'completed_status':FoodWastage.objects.filter(branch__pk=branch, date__date=date, status=True, delete=False).exists()},
-                    'oil_consumption':{'id':4, 'name':"oil_consumption", 'completed_status':FoodWastage.objects.filter(branch__pk=branch, date__date=date, status=True, delete=False).exists()},
+                    'food_wastage':{'id':9, 'name':"food_wastage", 'completed_status':FoodWastage.objects.filter(branch__pk=branch, date__date=date, status=True, delete=False).exists()},
+                    'oil_consumption':{'id':9, 'name':"oil_consumption", 'completed_status':FoodWastage.objects.filter(branch__pk=branch, date__date=date, status=True, delete=False).exists()},
+                    'vegetable_purchase':{'id':4, 'name':"vegetable_purchase", 'completed_status':InventoryControl.objects.filter(branch__pk=branch, date__date=date, status=True, delete=False, product__classification__code=4).exists()},
                 }
         bills_data = {
                     'free_bills': {'name':"free_bills", 'completed_status':FreeBill.objects.filter(branch__pk=branch, date__date=date, status=True, delete=False).exists()},
@@ -1294,7 +1293,6 @@ class DailySheetInventoryListSerializer(serializers.Serializer):
         return {
             'data': l
         }
-
 
 
 class ProductInstockListSerializer(serializers.ModelSerializer):
