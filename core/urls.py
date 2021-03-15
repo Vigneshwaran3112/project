7a11f865ab0bccab0ed8832a99c1b863c746b759
kabil <kabil@vishgyana.com>
2021-03-15 18:32:00 +0530

    .

--- core/urls.py
+++ core/urls.py
@@ -137,6 +137,7 @@ urlpatterns = [
 
     path('food_wastage_list/<str:date>/', views.FoodWastageList.as_view()),
 
+
     path('raw_operational_product_list/', views.RawOperationalProductList.as_view()),
 
     path('branch_specific_user_list/', views.BranchSpecificUserListAPIView.as_view()),
