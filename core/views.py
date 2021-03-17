5201ca08c12db0d6c69679fe27e3c973da03a3d9
vigneshwaran <vigneshwaran@vgts.tech>
2021-03-17 10:33:36 +0530

    .

--- core/views.py
+++ core/views.py
@@ -386,8 +386,6 @@ class UserRoleListAPIView(generics.ListAPIView):
     serializer_class = RolesSerializer
 
     def get_queryset(self):
-        user = BaseUser.objects.filter(pk=self.kwargs['pk']).exclude(employee_role__code__in=[1]).distinct()
-        print(user)
         return user
 
 
