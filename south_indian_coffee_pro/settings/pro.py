230ce42e81386f8ac959fb01bc61256ba16786b8
nandavgts <80396764+nandavgts@users.noreply.github.com>
2021-04-01 14:27:23 +0530

    Production

--- south_indian_coffee_pro/settings/pro.py
+++ south_indian_coffee_pro/settings/pro.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['tsich.api.vgts.tech']
 
@@ -14,5 +14,17 @@ DATABASES = {
         'PORT': '5432'
     }
 }
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+SECURE_SSL_REDIRECT = True
+
+SECURE_HSTS_SECONDS = 86400
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 CORS_ORIGIN_WHITELIST = ('https://tsich.vgts.tech', )
--- south_indian_coffee_pro/urls.py
+++ south_indian_coffee_pro/urls.py
@@ -4,8 +4,7 @@ from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('southindiancoffee/api-admin/', admin.site.urls),
     path('v1/', include('core.urls')),
     path('api-docs/', include_docs_urls(title='South Indian Coffee V1 APIs')),
-
 ]
