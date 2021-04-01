1a2d194b26cd808b575e63a8993965f264e89a66
nandavgts <80396764+nandavgts@users.noreply.github.com>
2021-04-01 12:22:27 +0530

    Altered Settings files for Development, Testing, Staging and Production

--- south_indian_coffee_pro/settings/base.py
+++ south_indian_coffee_pro/settings/base.py
@@ -4,10 +4,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = '@q^^01m#^v3u1*xpl-00m5h=0$n5c_@d)*_7%mp*v&@#(%rn$$'
 
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -18,7 +14,9 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
-    'core'
+    'django_crontab',
+    'core',
+    'import_export'
 ]
 
 MIDDLEWARE = [
@@ -34,13 +32,18 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'south_indian_coffee_pro.urls'
 
-REST_FRAMEWORK = { 
+CRONJOBS = [
+    ('0 2 * * *', 'core.views.my_cron_job')
+]
+
+
+REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication', ],
     'NON_FIELD_ERRORS_KEY': 'message',
     'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer', ],
     'DEFAULT_PARSER_CLASSES': ['rest_framework.parsers.JSONParser', ]
-    }
+}
 
 TEMPLATES = [
     {
@@ -60,24 +63,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'south_indian_coffee_pro.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'OPTIONS': {'options': '-c search_path=production'},
-        'NAME': 'south_indian_coffee_test',
-        'USER': 'dbmasteruser',
-        'PASSWORD': 'S0S-GfhoBlIrIm.#-L97.z-R7I9J-8I%',
-        'HOST': 'ls-db8b34df60a76c46c35bf55d1ffa56efbe9596b2.cjzymyxx2u8h.ap-south-1.rds.amazonaws.com',
-        'PORT': '5432'
-    }
-}
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
 ]
+
 
 AUTH_USER_MODEL = 'core.BaseUser'
 
@@ -98,13 +90,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = (
-    'http://127.0.0.1:8080',
-    'http://localhost:8080',
-    'http://127.0.0.1:3000',
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'http://localhost:3002',
-    'http://localhost:3002',
-    'http://sich.vgts.tech'
-)
+
+IMPORT_EXPORT_USE_TRANSACTIONS = True
